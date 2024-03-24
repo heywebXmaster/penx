67837c542e6c091996004dd435911f8fefeecbe8
AlbertTangJ <albert@tribe3.xyz>
2024-03-24 16:05:05 +0800

    build: create mobile version using ionic

---
+++ apps/mobile/src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import '~/styles/globals.css';
+import { useEffect } from 'react';
+import { set } from 'idb-keyval';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { ToastContainer } from 'uikit';
+import { appEmitter, initFower } from '@penx/app';
+import { PENX_SESSION_USER } from '@penx/constants';
+import { store, StoreProvider } from '@penx/store';
+import { TrpcProvider } from '@penx/trpc-client';
+import { ClientOnly } from '../components/ClientOnly';
+import '@glideapps/glide-data-grid/dist/index.css';
+
+initFower();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { push } = useRouter();
+  useEffect(() => {
+    const handleSignOut = () => {
+      set(PENX_SESSION_USER, null);
+      store.setToken(null as any);
+      push('/');
+    };
+
+    appEmitter.on('SIGN_OUT', handleSignOut);
+    return () => {
+      appEmitter.off('SIGN_OUT', handleSignOut);
+    };
+  }, []);
+
+  return (
+    <ClientOnly>
+      <StoreProvider>
+        <TrpcProvider>
+          <ToastContainer position="bottom-right" />
+          <Component {...pageProps} />
+        </TrpcProvider>
+      </StoreProvider>
+    </ClientOnly>
+  );
+}
+
+export default MyApp;
